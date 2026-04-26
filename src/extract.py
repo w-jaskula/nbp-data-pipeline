91170d2371ce028301d51ed44d490139bc33568e
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-04-26 14:42:32 +0200

    extract Q1 currency url

--- src/extract.py
+++ src/extract.py
@@ -7,19 +7,16 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
 
 path = os.getenv("NBP_API_URL")
-def fetch_data(api_url, table="a"):
+def fetch_data(api_url, currency, start_date, end_date):
     try:
-        full_url = f"{api_url}{table}/"
+        full_url = f"{api_url}/{currency}/{start_date}/{end_date}"
         response = requests.get(full_url)
         if response.status_code == 200:
             data = response.json()
             logging.info(f"Successfully fetched data from {full_url}")
-            return data[0]
+            return data
         else:
             logging.error(f"Failed to fetch data from {full_url}. Status code: {response.status_code}")
             return None
     except requests.exceptions.RequestException as e:
         logging.error(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    raw_data = fetch_data(path)
