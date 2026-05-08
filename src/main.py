e1eaf97fd62502bcfd84b545200dbb2c0d079637
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-08 20:07:48 +0200

    add multi-currency support in pipeline

--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from extract import fetch_data
 from transform import transform_data
-from load import save_to_csv
+from load import save_to_db
 
 load_dotenv()
 
 def main():
     api_url = os.getenv("NBP_API_URL")
 
-    data = fetch_data(api_url, "EUR", "2026-01-01", "2026-03-31")
-    df = transform_data(data)
-    save_to_csv(df, "../data/nbp_data.csv")
+    eur_data = fetch_data(api_url, "EUR", "2026-01-01", "2026-03-31")
+    usd_data = fetch_data(api_url, "USD", "2026-01-01", "2026-03-31")
+    gbp_data = fetch_data(api_url, "GBP", "2026-01-01", "2026-03-31")
+
+    df_eur = transform_data(eur_data)
+    df_usd = transform_data(usd_data)
+    df_gbp = transform_data(gbp_data)
+
+    df = pd.concat([df_eur,df_usd,df_gbp])
+    save_to_db(df, "../data/nbp.db")
 
 if __name__ == "__main__":
     main()
