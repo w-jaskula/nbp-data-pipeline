8a8f49cfb885b03ac44b21916ce621ddfa0490b7
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-08 14:13:58 +0200

    add initial transformation logic

--- src/transform.py
+++ src/transform.py
@@ -2,3 +2,42 @@ import pandas as pd
 from src.extract import fetch_data
 
 def transform_data(data):
+    transformed_data = []
+
+    rates = data["rates"]
+    currency = data["code"]
+
+    for record in rates:
+        date = record["effectiveDate"]
+        rate = record["mid"]
+
+        transformed_data.append({
+            "date": date,
+            "currency": currency,
+            "rate": rate
+        })
+
+    df = pd.DataFrame(transformed_data)
+
+    # Sorting by date
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.sort_values("date")
+
+    # SMA7
+    df["sma7"] = df["rate"].rolling(window=7).mean()
+
+    # % change
+    df["pct_change"] = df["rate"].pct_change() * 100
+
+    # Define signal
+    def get_signal(x):
+        if x > 1:
+            return "increase"
+        elif x < -1:
+            return "drop"
+        else:
+            return "stable"
+
+    df["signal"] = df["pct_change"].apply(get_signal)
+
+    return df
