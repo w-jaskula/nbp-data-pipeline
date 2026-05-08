4a6343e9183f510c38490f346ea54dce04d92a2f
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-08 18:37:16 +0200

    add sqlite data loading

--- src/load.py
+++ src/load.py
@@ -1,3 +1,6 @@
-def save_to_csv(df, filename="nbp_data.csv"):
-    df.to_csv(filename, index=False)
-    return filename
+import sqlite3
+
+def save_to_db(df, db_path="../data/nbp.db"):
+    conn = sqlite3.connect(db_path)
+    df.to_sql("rates", conn, if_exists="replace", index=False)
+    conn.close()
