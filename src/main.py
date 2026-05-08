831aa09b4e1a49370b9d6c4c43c6dc113445c311
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-08 16:39:09 +0200

    add pipeline orchestration in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-# Draft
+import os
+from dotenv import load_dotenv
 from extract import fetch_data
-from transform import clean_data
+from transform import transform_data
 from load import save_to_csv
 
+load_dotenv()
+
 def main():
-    data = fetch_data(...)
-    clean = clean_data(data)
-    save_to_csv(clean, ...)
+    api_url = os.getenv("NBP_API_URL")
+
+    data = fetch_data(api_url, "EUR", "2026-01-01", "2026-03-31")
+    df = transform_data(data)
+    save_to_csv(df, "../data/nbp_data.csv")
 
 if __name__ == "__main__":
     main()
