a4ff37ba06d8cefbb9b7294dc65b6b9ecc278e23
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-12 21:09:56 +0200

    add logger to extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,9 +1,9 @@
 import requests
-import logging
+from utils import get_logger
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
+logger = get_logger(__name__)
 
 def fetch_data(api_url, currency, start_date, end_date):
     try:
@@ -11,10 +11,10 @@ def fetch_data(api_url, currency, start_date, end_date):
         response = requests.get(full_url)
         if response.status_code == 200:
             data = response.json()
-            logging.info(f"Successfully fetched data from {full_url}")
+            logger.info(f"Successfully fetched data from {full_url}")
             return data
         else:
-            logging.error(f"Failed to fetch data from {full_url}. Status code: {response.status_code}")
+            logger.error(f"Failed to fetch data from {full_url}. Status code: {response.status_code}")
             return None
     except requests.exceptions.RequestException as e:
-        logging.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
