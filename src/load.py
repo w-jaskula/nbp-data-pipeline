ba501e3bc64e124246d8b9ab23189270f6895013
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-12 22:24:59 +0200

    add logger to load.py

--- src/load.py
+++ src/load.py
@@ -1,6 +1,14 @@
 import sqlite3
+from utils import get_logger
+
+logger = get_logger(__name__)
 
 def save_to_db(df, db_path="../data/nbp.db"):
     conn = sqlite3.connect(db_path)
+
+    logger.info(f"Saving {len(df)} records to database at {db_path}")
+
     df.to_sql("rates", conn, if_exists="replace", index=False)
     conn.close()
+
+    logger.info("Database update completed successfully")
