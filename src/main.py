d357f0349fb29b8b07b5803b6f3cc1e365a98cfa
w-jaskula <99075861+w-jaskula@users.noreply.github.com>
2026-05-12 22:27:04 +0200

    add logger to main.py

--- src/main.py
+++ src/main.py
@@ -4,12 +4,16 @@ from dotenv import load_dotenv
 from extract import fetch_data
 from transform import transform_data
 from load import save_to_db
+from utils import get_logger
 
 load_dotenv()
+logger = get_logger(__name__)
 
 def main():
+    logger.info("Starting NBP Currency Pipeline")
     api_url = os.getenv("NBP_API_URL")
 
+    logger.info("Fetching data for EUR, USD, and GBP...")
     eur_data = fetch_data(api_url, "EUR", "2026-01-01", "2026-03-31")
     usd_data = fetch_data(api_url, "USD", "2026-01-01", "2026-03-31")
     gbp_data = fetch_data(api_url, "GBP", "2026-01-01", "2026-03-31")
@@ -18,8 +22,11 @@ def main():
     df_usd = transform_data(usd_data)
     df_gbp = transform_data(gbp_data)
 
+    logger.info("Merging datasets and saving to database...")
     df = pd.concat([df_eur,df_usd,df_gbp])
     save_to_db(df, "../data/nbp.db")
+
+    logger.info("Pipeline finished successfully")
 
 if __name__ == "__main__":
     main()
